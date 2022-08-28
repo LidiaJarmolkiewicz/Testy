df8d8cc468cd9bde3126fcc88cd16c70cd75fd4d
LidiaJarmolkiewicz <lidiajarmolkiewicz@gmail.com>
2022-08-28 22:02:12 +0200

    TriangleClass

--- TriangleClassTest/test.cpp
+++ TriangleClassTest/test.cpp
@@ -9,13 +9,38 @@ TEST(TestTriangle, TestSidePositive) {
   EXPECT_GT(triangle.getSideC(), 0);
 }
 
-TEST(TestTriangle, TestPerimeter) {
+TEST(TestTriangle, getPerimeterValidTriange) {
 	Triangle triangle(3, 4, 5);
 	
 	EXPECT_EQ(triangle.getPerimeter(),12);
 }
-TEST(TestTriangle, TestArea) {
+
+TEST(TestTriangle, getPerimeterInValidTriange) {
+	Triangle triangle(1,2,4);
+
+	EXPECT_EQ(triangle.getPerimeter(), 0);
+}
+
+
+TEST(TestAreaTriangle,getAreaValidTriange) {
 	Triangle triangle(3, 4, 5);
 
-	EXPECT_EQ(triangle.getArea(), 6);
+	EXPECT_FLOAT_EQ(triangle.getArea(), 6);
+}
+TEST(TestAreaTriangle, getAreaInValidTriange) {//dla parametrow 1,2,3 grozi bledem testowania bo zwraca 0 przypadkowo trafilismy w dobre wartosci
+	Triangle triangle(1,2,3);
+
+	EXPECT_FLOAT_EQ(triangle.getArea(), 0);
+}
+
+TEST(TestValidityTest, IsValidTriange_returnTrue) {
+	Triangle triangle(3,4,5);
+
+	EXPECT_TRUE(triangle.isValid());
+}
+
+TEST(TestValidityTest, IsInValidTriange_returnFalse) {
+	Triangle triangle(1, 2, 3);
+
+	EXPECT_FALSE(triangle.isValid());
 }
